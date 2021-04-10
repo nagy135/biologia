3038bef88d0b6f4519ed0edbbe29cc188acf1c8c
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-10 22:45:58 +0200

    feat: cleanup level blade

--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 <div class="about">
     <div class="container">
-    <h1>{{ $level->name }}</h1>
+    <h1 class="level-name">{{ $level->name }}</h1>
     @foreach($level->groups as $group)
-        <h3>{{ $group->name }}</h3>
+        <h3 class="group-name">{{ $group->name }}</h3>
         <ul class="list-group">
             @if($group->documents->count())
                 @foreach($group->documents as $document)
                     <li class="list-group-item">
                         <a href="{{ asset('storage/' . $document->file) }}" target="_blank">
-                            <button class="btn"><i class="fa fa-download"></i> {{ $document->name }}</button>
+                            <button class="btn"><i class="fa fa-download"></i> {{ $document->name }}</span></button>
                         </a>
                     </li>
                 @endforeach
