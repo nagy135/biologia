63eeaf33276fd9744f0bc1062577bfc24ecaf4ee
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-16 21:56:49 +0200

    feat: blade @if($level)

--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -3,23 +3,27 @@
 @section('content')
 <div class="about">
     <div class="container">
-    <h1 class="level-name">{{ $level->name }}</h1>
-    @foreach($level->groups as $group)
-        <h3 class="group-name">{{ $group->name }}</h3>
-        <ul class="list-group">
-            @if($group->documents->count())
-                @foreach($group->documents as $document)
-                    <li class="list-group-item">
-                        <a href="{{ asset('storage/' . $document->file) }}" target="_blank">
-                            <button class="btn"><i class="fa fa-download"></i> {{ $document->name }}</span></button>
-                        </a>
-                    </li>
-                @endforeach
-            @else
-                <li class="list-group-item">prazdne...</li>
-            @endif
-        </ul>
-    @endforeach
+        @if($level)
+            <h1 class="level-name">{{ $level->name }}</h1>
+            @foreach($level->groups as $group)
+                <h3 class="group-name">{{ $group->name }}</h3>
+                <ul class="list-group">
+                    @if($group->documents->count())
+                        @foreach($group->documents as $document)
+                            <li class="list-group-item">
+                                <a href="{{ asset('storage/' . $document->file) }}" target="_blank">
+                                    <button class="btn"><i class="fa fa-download"></i> {{ $document->name }}</span></button>
+                                </a>
+                            </li>
+                        @endforeach
+                    @else
+                        <li class="list-group-item">prazdne...</li>
+                    @endif
+                </ul>
+            @endforeach
+        @else
+            <h2>Ročník neexistuje</h2>
+        @endif
     </div>
 </div>
 @endsection
