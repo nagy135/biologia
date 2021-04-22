a204883f4563495151382ce4a8f803e72781830b
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-22 21:53:30 +0200

    fix: dont force https locally

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-\URL::forceScheme('https');
+if (config('app.env') != 'local') \URL::forceScheme('https');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
